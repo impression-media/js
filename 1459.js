5e886de15f22dd01bd3aea2d5f907d7203467737
impression-media <86666704+impression-media@users.noreply.github.com>
2023-05-10 11:03:36 +0200

    Automated update

--- 1459.js
+++ 1459.js
@@ -1,4 +1,4 @@
-/* Version: V_06_b - 28.02. 2023 - 17:52:40 */
+/* Version: V_06_d - 10.05. 2023 - 11:03:24 */
 /* installedModules: adform,appnexus,criteo,rubicon,consentManagementGDPR,pubCommonIdSystem,criteoIdSystem,schain,sharedIdSystem*/
 /* prebid.js v6.5.0
 Updated : 2022-02-01*/
@@ -29,30 +29,48 @@ var imHbWonBids = imHbWonBids || [];
 var imHbAsocPlacementMidEnable = imHbAsocPlacementMidEnable || [];
 var imHbPlacementEnable = imHbPlacementEnable || [];
 
-            function imGetCookie(name) {var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)'); return v ? v[2] : null;}; function imSetCookie(name,value,days){var d=new Date;d.setTime(d.getTime()+24*60*60*1000*days);document.cookie=name+'='+value+';domain=.;path=/;expires='+d.toGMTString();};function imTestingParam(t){var n=null,e=[];return location.search.substr(1).split('&').forEach(function(o){(e=o.split('='))[0]===t&&(n=decodeURIComponent(e[1]))}),n};var imtesting = imTestingParam('imtesting');if(imtesting=='start'){imSetCookie('imtesting','dev',1);}if(imtesting=='stop'){imSetCookie('imtesting', '', -1);}function imRemoveTestInfo(){document.getElementById('imtestingInfo').remove();}function imHbUploadConfig(){if(imGetCookie('imtesting')){document.getElementById('imtestingInfo')&&document.getElementById('imtestingInfo').remove();var infoImTesting=document.createElement('div');infoImTesting.id='imtestingInfo',infoImTesting.innerHTML='<a href="https://hb.impressionmedia.cz/administrace/pages/weby.php?openId=0" style="text-derocation:none; color:white;">TESTING MODE</a><span onclick="imRemoveTestInfo();" style="cursor: pointer;border: 1px solid white;border-radius: 20px 2px 2px;padding: 4px 6px 1px 10px;position: absolute; bottom: 2px;right: 2px;">X</span>',infoImTesting.style.cssText="font-size:12px;line-height:18px;z-index:999999;position:fixed;bottom:3px;right:3px;margin-top:-30px;padding:65px 15px 50px 45px;background:#0000003d;color:white;border-radius:3px;border-top-left-radius:200px;border:1px solid white;box-shadow:1px 1px 1px black;",document.body.appendChild(infoImTesting);;var imConfigId = '0'; var imConfigName = 'HBsetup_'}else{var imConfigId = '1459'; var imConfigName = 'HBsetup_wwwjimetocz_Jimeto_cz___aktivni_PlacementLazyLoading__od_17_1__2_22__2023_02_28_1752'};var imHbScript = document.createElement('script');imHbScript.type = 'text/javascript';imHbScript.id = 'imHbConfig';var imHbRandomParam = Math.floor(Math.random() * 10000) + 1;imHbScript.src = 'https://cdn.jsdelivr.net/gh/impression-media/js/tmp/js/'+imConfigId+'/'+imConfigName+'.min.js?imHbRandomParam='+imHbRandomParam;document.getElementsByTagName('head')[0].appendChild(imHbScript);}
+            function imGetCookie(name) {var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)'); return v ? v[2] : null;}; function imSetCookie(name,value,days){var d=new Date;d.setTime(d.getTime()+24*60*60*1000*days);document.cookie=name+'='+value+';domain=.;path=/;expires='+d.toGMTString();};function imTestingParam(t){var n=null,e=[];return location.search.substr(1).split('&').forEach(function(o){(e=o.split('='))[0]===t&&(n=decodeURIComponent(e[1]))}),n};var imtesting = imTestingParam('imtesting');if(imtesting=='start'){imSetCookie('imtesting','dev',1);}if(imtesting=='stop'){imSetCookie('imtesting', '', -1);}function imRemoveTestInfo(){document.getElementById('imtestingInfo').remove();}function imHbUploadConfig(){if(imGetCookie('imtesting')){document.getElementById('imtestingInfo')&&document.getElementById('imtestingInfo').remove();var infoImTesting=document.createElement('div');infoImTesting.id='imtestingInfo',infoImTesting.innerHTML='<a href="https://hb.impressionmedia.cz/administrace/pages/weby.php?openId=0" style="text-derocation:none; color:white;">TESTING MODE</a><span onclick="imRemoveTestInfo();" style="cursor: pointer;border: 1px solid white;border-radius: 20px 2px 2px;padding: 4px 6px 1px 10px;position: absolute; bottom: 2px;right: 2px;">X</span>',infoImTesting.style.cssText="font-size:12px;line-height:18px;z-index:999999;position:fixed;bottom:3px;right:3px;margin-top:-30px;padding:65px 15px 50px 45px;background:#0000003d;color:white;border-radius:3px;border-top-left-radius:200px;border:1px solid white;box-shadow:1px 1px 1px black;",document.body.appendChild(infoImTesting);;var imConfigId = '0'; var imConfigName = 'HBsetup_'}else{var imConfigId = '1459'; var imConfigName = 'HBsetup_wwwjimetocz_Jimeto_cz___aktivni_PlacementLazyLoading__od_17_1__2_22__2023_05_10_1103'};var imHbScript = document.createElement('script');imHbScript.type = 'text/javascript';imHbScript.id = 'imHbConfig';var imHbRandomParam = Math.floor(Math.random() * 10000) + 1;imHbScript.src = 'https://cdn.jsdelivr.net/gh/impression-media/js/tmp/js/'+imConfigId+'/'+imConfigName+'.min.js?imHbRandomParam='+imHbRandomParam;document.getElementsByTagName('head')[0].appendChild(imHbScript);}
+    //posledni cyklus nepromazavat    
+    var selection = document.body.getAttribute('data-hbmasterscript') !== null;
+    if (selection) {       
+        hbmasterscript = document.body.getAttribute('data-hbmasterscript'); 
+        var hbLastMasterDigit = parseInt(hbmasterscript.replace('hb-', ''),10);
+        var hbLastCycle = (15 - (hbLastMasterDigit + 1)); 
+    }else{
+        var hbLastCycle = 15;  
+    }
+    console.log('hbLastCycle: '+hbLastCycle);
+    if(hbLastCycle <= 0){
+        //OK uz nic nemaz, uz dalsi klasicky reloading neprijde
+        console.log('OK uz nic nemaz, uz dalsi klasicky reloading neprijde');
+    }else{
+        setTimeout(function(){
+            console.log('imHbPlacementEnable^^');
+            console.log(imHbPlacementEnable);
 
-    setTimeout(function(){
-        console.log('imHbPlacementEnable^^');
-        console.log(imHbPlacementEnable);
+            console.log('imHbAsocPlacementMidEnable^^');
+            console.log(imHbAsocPlacementMidEnable);
 
-        console.log('imHbAsocPlacementMidEnable^^');
-        console.log(imHbAsocPlacementMidEnable);
+            for (var key in imHbAsocPlacementMidEnable) {
+                console.log('imHbAsocPlacementMidEnable key: '+ key);
+                console.log(imHbAsocPlacementMidEnable[key]);
+                  var resetMid = imHbAsocPlacementMidEnable[key];
+                  if(imHbPlacementEnable.includes(key)){
+                  
+                        console.log('resetTargeting resetMid: '+resetMid);
+                        adformtag.resetTargeting(resetMid);
+                        
+                  }    
+            }
+            pbjs = {};
+            pbjs.que = [];
+            document.body.setAttribute('im-pr', '0');
+            document.body.setAttribute('data-posRel', 'Active: ');
+            document.body.setAttribute('s-pr', 'start');
 
-        for (var key in imHbAsocPlacementMidEnable) {
-            console.log('imHbAsocPlacementMidEnable key: '+ key);
-            console.log(imHbAsocPlacementMidEnable[key]);
-              var resetMid = imHbAsocPlacementMidEnable[key];
-              if(imHbPlacementEnable.includes(key)){
-              
-                    console.log('resetTargeting resetMid: '+resetMid);
-                    adformtag.resetTargeting(resetMid);
-                    
-              }    
-        }
-        pbjs = {};
-        pbjs.que = [];
-        
-    }, (50000 - 10000));
+        }, (50000 - 10000));
+
+    }
 
     document.currentScript.setAttribute('id', 'imHbMaster');
     var selection = document.body.getAttribute('data-hbmasterscript') !== null;
@@ -73,6 +91,7 @@ var imHbPlacementEnable = imHbPlacementEnable || [];
         //prvni nacteni configu
         setTimeout(function(){
             imHbUploadConfig();
+        
         }, 10);
 
         //vse smaz a pust novy uploud HB ovladacu 
