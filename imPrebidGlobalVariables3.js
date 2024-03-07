814fe0ae25250820eccd21f4074d883d2d2fa988
impression-media <86666704+impression-media@users.noreply.github.com>
2024-03-07 21:41:52 +0100

    Automated update

--- imPrebidGlobalVariables3.js
+++ imPrebidGlobalVariables3.js
@@ -1,4 +1,4 @@
-/* Version: G_03 - 07.03. 2024 - 20:23:47 */
+/* Version: G_03 - 07.03. 2024 - 21:41:48 */
 var dealIdsPercent = { 
     "DID-1665-101195": 0.200,
     "DID-1665-116790": 0.200,
@@ -141,8 +141,4 @@ var globalSasCPM = {    "meteocentrumcz1649leaderboardadformsasAll": "4",
     "netflixercz1732adformMobilniBrandingadformsasAll": "1.61",
     "pisnickyakordycz1733adformsquareadformsasAll": "1.09"};
 
-var globalExternSasCPM = {
-"pisnickyakordyczadformsquare": "1.08",
-"pisnickyakordyczadformsquare": "1.08",
-"pisnickyakordyczadformsquare": "1.08"
-};
+var globalExternSasCPM = {"pisnickyakordyczadformsquare": "1.07","pisnickyakordyczadformsquare": "1.08","pisnickyakordyczadformsquare": "1.09"};
