06092fcefa4d0a3a79ab6fb02057abe1d13e4eaa
impression-media <86666704+impression-media@users.noreply.github.com>
2024-03-20 02:45:09 +0100

    Automated update

--- imPrebidGlobalVariables3.js
+++ imPrebidGlobalVariables3.js
@@ -1,4 +1,4 @@
-/* Version: G_03 - 20.03. 2024 - 01:45:01 */
+/* Version: G_03 - 20.03. 2024 - 02:45:01 */
 var dealIdsPercent = { 
     "DID-1665-101195": 0.200,
     "DID-1665-116790": 0.200,
@@ -144,6 +144,11 @@ var globalSasCPM = {    "meteocentrumcz1649leaderboardadformsasAll": "4",
     "autacz174adformMobilniBrandingadformsasAll": "1.61",
     "autacz174adformsquareadformsasAll": "1.09",
     "autacz174adformleaderboardadformsasAll": "1.09",
-    "autacz174adformleaderboardadformsasBranding": "3.04"};
+    "autacz174adformleaderboardadformsasBranding": "3.04",
+    "hybridcz1741adformsquareadformsasAll": "1.09",
+    "hybridcz1741adformleaderboardadformsasAll": "1.09",
+    "hybridcz1741adformleaderboardadformsasBranding": "3.04",
+    "hybridcz1741adformMobilniBrandingadformsasAll": "1.61",
+    "hybridcz1741AdTrackGenericInterscrollerMobileadformsasAll": "2.09"};
 
 var globalExternSasCPM = {"pisnickyakordyczadformsquare": "1.05","pisnickyakordyczadformsquare": "1.08","pisnickyakordyczadformsquare": "1.09","autowebczadformleaderboard": "1.09","autowebczadformsquare": "1.09","autowebczadformsquare2": "1.09","autowebczadformsquare3": "1.09","autowebczadformrectangle": "1.09","ifaunaczdivgptadifatf": "1.09","ifaunaczdivgptadifskyskraper1": "1.09","ifaunaczdivgptadifskyskraper2": "1.09","ifaunaczdivgptadifskyskraper3": "1.09"};
