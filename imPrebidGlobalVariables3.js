00716224e4f4edf9a7b5c4c148e0ef90c3e1aedb
impression-media <86666704+impression-media@users.noreply.github.com>
2024-07-15 21:45:08 +0200

    Automated update

--- imPrebidGlobalVariables3.js
+++ imPrebidGlobalVariables3.js
@@ -1,4 +1,4 @@
-/* Version: G_03 - 15.07. 2024 - 20:45:01 */
+/* Version: G_03 - 15.07. 2024 - 21:45:02 */
 var dealIdsPercent = { 
     "DID-1665-101195": 0.200,
     "DID-1665-116790": 0.200,
@@ -106,7 +106,7 @@ var bidderFloor = {
     "adform-prima": 10,
     "adform-sasAll": 3,
     "adform-sasBranding": 3,
-    "Iprima": 10
+    "iprima": 10
 };
 
 var globalSasCPM = {    "serialzonecz1574topleaderboarddesktopadformsasAll": "1.09",
