086385d271bf844904f0ecf40793acd78377804e
impression-media <86666704+impression-media@users.noreply.github.com>
2024-11-15 03:45:06 +0100

    Automated update

--- imPrebidGlobalVariables3.js
+++ imPrebidGlobalVariables3.js
@@ -1,4 +1,4 @@
-/* Version: G_03 - 15.11. 2024 - 02:45:01 */
+/* Version: G_03 - 15.11. 2024 - 03:45:01 */
 var dealIdsPercent = { 
     "DID-1665-101195": 0.200,
     "DID-1665-116790": 0.200,
@@ -437,6 +437,16 @@ var globalSasCPM = {    "serialzonecz1574topleaderboarddesktopadformsasAll": "1
     "nasetemacz1811adformrectangleadformsasAll": "1.09",
     "nasetemacz1811adformsquarem2adformsasAll": "1.09",
     "nasetemacz1811AdTrackGenericInterscrollerMobileadformsasAll": "2.17",
-    "nasetemacz1811adformMobilniBrandingadformsasAll": "1.61"};
+    "nasetemacz1811adformMobilniBrandingadformsasAll": "1.61",
+    "nasetemacz1815adformleaderboardadformsasAll": "1.09",
+    "nasetemacz1815adformleaderboardadformsasBranding": "3.04",
+    "nasetemacz1815adformhalfpage2adformsasAll": "1.09",
+    "nasetemacz1815adformhalfpageadformsasAll": "1.09",
+    "nasetemacz1815adformleaderboard2adformsasAll": "1.09",
+    "nasetemacz1815adformsquaremadformsasAll": "1.09",
+    "nasetemacz1815adformrectangleadformsasAll": "1.09",
+    "nasetemacz1815adformsquarem2adformsasAll": "1.09",
+    "nasetemacz1815AdTrackGenericInterscrollerMobileadformsasAll": "2.17",
+    "nasetemacz1815adformMobilniBrandingadformsasAll": "1.61"};
 
 var globalExternSasCPM = {"pisnickyakordyczadformsquare": "1.05","pisnickyakordyczadformsquare": "1.08","pisnickyakordyczadformsquare": "1.09","autowebczadformleaderboard": "1.09","autowebczadformsquare": "3.04","autowebczadformsquare2": "1.09","autowebczadformsquare3": "1.09","autowebczadformrectangle": "1.09","ifaunaczdivgptadifatf": "1.09","ifaunaczdivgptadifskyskraper1": "1.09","ifaunaczdivgptadifskyskraper2": "1.09","ifaunaczdivgptadifskyskraper3": "1.09","autowebczadforminterscroller": "2.09","autowebczadformleaderboard": "5.04","autowebczadformsquare": "1.09","autowebczadformleaderboard": "1.09","zakrutaczadformleaderboard": "1.09","zakrutaczadformleaderboard": "3.26","zakrutaczadformsquare": "1.09","zakrutaczadformrectangle": "1.09","aahbimpressionmediaczadformsquare": "1.09"};
