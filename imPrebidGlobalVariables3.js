8d56f7f3a8c20d0a200191d5792c84bbd6beabdc
impression-media <86666704+impression-media@users.noreply.github.com>
2024-12-25 03:45:06 +0100

    Automated update

--- imPrebidGlobalVariables3.js
+++ imPrebidGlobalVariables3.js
@@ -1,4 +1,4 @@
-/* Version: G_03 - 25.12. 2024 - 02:45:01 */
+/* Version: G_03 - 25.12. 2024 - 03:45:01 */
 var dealIdsPercent = { 
     "DID-1665-101195": 0.200,
     "DID-1665-116790": 0.200,
@@ -477,6 +477,9 @@ var globalSasCPM = {    "serialzonecz1574topleaderboarddesktopadformsasAll": "1
     "kinotip2cz1822adformrectangleadformsasAll": "1.09",
     "kinotip2cz1822adformleaderboardadformsasAll": "1.09",
     "kinotip2cz1822adformleaderboardadformsasBranding": "3.04",
-    "kinotip2cz1822AdTrackGenericInterscrollerMobileadformsasAll": "2.17"};
+    "kinotip2cz1822AdTrackGenericInterscrollerMobileadformsasAll": "2.17",
+    "radiacz1828adformleaderboardadformsasBranding": "3.04",
+    "radiacz1828adformsquare2adformsasAll": "1.09",
+    "radiacz1828AdTrackGenericInterscrollerDesktopadformsasAll": "2.17"};
 
 var globalExternSasCPM = {"pisnickyakordyczadformsquare": "1.05","pisnickyakordyczadformsquare": "1.08","pisnickyakordyczadformsquare": "1.09","autowebczadformleaderboard": "1.09","autowebczadformsquare": "3.04","autowebczadformsquare2": "1.09","autowebczadformsquare3": "1.09","autowebczadformrectangle": "1.09","ifaunaczdivgptadifatf": "1.09","ifaunaczdivgptadifskyskraper1": "1.09","ifaunaczdivgptadifskyskraper2": "1.09","ifaunaczdivgptadifskyskraper3": "1.09","autowebczadforminterscroller": "2.09","autowebczadformleaderboard": "5.04","autowebczadformsquare": "1.09","autowebczadformleaderboard": "1.09","zakrutaczadformleaderboard": "1.09","zakrutaczadformleaderboard": "3.26","zakrutaczadformsquare": "1.09","zakrutaczadformrectangle": "1.09","aahbimpressionmediaczadformsquare": "1.09"};
