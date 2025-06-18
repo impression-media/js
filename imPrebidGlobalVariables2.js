e0447e50eb16337b76eab32be53702b47b45d236
impression-media <86666704+impression-media@users.noreply.github.com>
2025-06-18 03:40:05 +0200

    Automated update

--- imPrebidGlobalVariables2.js
+++ imPrebidGlobalVariables2.js
@@ -1,4 +1,4 @@
-/* Version: G_02 - 18.06. 2025 - 02:40:01 */
+/* Version: G_02 - 18.06. 2025 - 03:40:02 */
 var dealIdsPercent = { 
     "DID-1665-101195": 0.200,
     "DID-1665-116790": 0.200,
@@ -138,6 +138,7 @@ var globalSasCPM = {    "serialzonecz1574topleaderboarddesktopadformsasAll": "0
     "serialzonecz1574topleaderboardbrandingadformsasBranding": "2.61",
     "serialzonecz1574square1adformsasAll": "1.09",
     "serialzonecz1574bottomleaderboardmobiladformsasAll": "0.74",
+    "nespechejcz1853adformrectangleadformsasAll": "1.09",
     "meteocentrumcz1649leaderboardadformsasAll": "4",
     "autanetcz1651AdTrackGenericInterscrollerMobileadformsasAll": "4",
     "prehrajto1676adformleaderboardadformsasAll": "4",
@@ -508,4 +509,8 @@ var globalSasCPM = {    "serialzonecz1574topleaderboarddesktopadformsasAll": "0
     "nespechejcz1852adformleaderboardadformsasAll": "0.74",
     "nespechejcz1852adformleaderboardadformsasBranding": "2.61",
     "nespechejcz1852adformhalfpageadformsasAll": "0.74",
-    "nespechejcz1852adformrectangleadformsasAll": "1.09"};
+    "nespechejcz1852adformrectangleadformsasAll": "1.09",
+    "nespechejcz1853AdTrackGenericInterscrollerMobileadformsasAll": "1.96",
+    "nespechejcz1853adformhalfpageadformsasAll": "0.74",
+    "nespechejcz1853adformleaderboardadformsasAll": "0.74",
+    "nespechejcz1853adformleaderboardadformsasBranding": "2.61"};
