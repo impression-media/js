c7d978a74efa253fda05c2673dd6119366e8f974
impression-media <86666704+impression-media@users.noreply.github.com>
2025-12-17 17:21:05 +0100

    Automated update

--- imPrebidGlobalVariables3.js
+++ imPrebidGlobalVariables3.js
@@ -1,4 +1,4 @@
-/* Version: G_03 - 17.12. 2025 - 16:21:01 */
+/* Version: G_03 - 17.12. 2025 - 17:21:01 */
 var dealIdsPercent = { 
     "DID-1665-101195": 0.200,
     "DID-1665-116790": 0.200,
@@ -141,7 +141,7 @@ var bidderFloor = {
     "iprima": 10,
     "setupad": 3,
     "criteo-cpex": 1,
-    "criteo-cpex2": 10
+    "criteo-cpex2": 2
 };
 
 var globalSasCPM = {    "meteocentrumcz1649leaderboardadformsasAll": "4",
