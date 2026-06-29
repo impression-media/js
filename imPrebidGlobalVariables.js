5d91f8ab78d3aea33a82c18042972f3a35dea725
impression-media <86666704+impression-media@users.noreply.github.com>
2026-06-29 18:16:09 +0200

    Automated update

--- imPrebidGlobalVariables.js
+++ imPrebidGlobalVariables.js
@@ -1,4 +1,4 @@
-/* Version: G_01 - 29.06. 2026 - 17:16:02 */
+/* Version: G_01 - 29.06. 2026 - 18:16:01 */
 var dealIdsPercent = { 
     "DID-1665-101195": 0.200,
     "DID-1665-116790": 0.200,
@@ -126,7 +126,6 @@ var bidderFloor = {
     "ix": 3,
     "adform-omd": 3,
     "Teads": 3,
-    "pubmatic-cpex": 3,
     "mgid": 3,
     "stroeerCore": 3,
     "appnexus-cpex": 3,
@@ -134,12 +133,14 @@ var bidderFloor = {
     "adform-im-nt": 3,
     "cpex-nt": 3,
     "adform-im-tt": 3,
-    "smilewanted-im": 3,
-    "adform-prima": 10,
-    "adform-sasAll": 3,
-    "adform-sasBranding": 3,
-    "iprima": 10,
-    "setupad": 3,
+    "pubmatic-cpex": 3,
+    "seznam": 3,
+    "criteo-cpex2": 2,
     "criteo-cpex": 1,
-    "criteo-cpex2": 2
+    "setupad": 3,
+    "iprima": 10,
+    "adform-sasBranding": 3,
+    "adform-sasAll": 3,
+    "adform-prima": 10,
+    "smilewanted-im": 3
 };
